442370512e1aef9bedb8aa58d09239c2b81035ef
Leonardo Castro <ldcmleo19@gmail.com>
2026-02-02 21:42:10 -0600

    new design for main page

--- src/components/main/Proyecto.tsx
+++ src/components/main/Proyecto.tsx
@@ -17,15 +17,28 @@ type DataProps = {
 };
 
 export default function Proyecto({ data }: { data: DataProps }) {
+  const totalSkills = data.skills.length;
+
+  function hasBorder(index: number, columns: number) {
+    const lastRows = totalSkills % columns;
+    if (lastRows === 0) return false;
+
+    if (index + columns >= totalSkills && index < totalSkills - lastRows)
+      return true;
+  }
+
   return (
-    <div key={data.id} className="md:flex gap-x-8 pb-8">
-      <div className="min-w-[120px]">
-        <span className="text-zinc-700 dark:text-zinc-400">{data.time}</span>
+    <div
+      key={data.id}
+      className="md:flex border-b border-[#a9b4c6] border-dashed"
+    >
+      <div className="min-w-[160px] border-b md:border-b-0 md:border-r border-[#a9b4c6] border-dashed p-4">
+        <span className="">{data.time}</span>
       </div>
       <div>
         <div>
-          <h1>{data.role}</h1>
-          <h2 className="text-zinc-700 dark:text-zinc-400">
+          <h1 className="px-4 pt-4">{data.role}</h1>
+          <h2 className="px-4">
             {data.at.isLink ? (
               <a href={data.at.link} target="_blank" rel="noreferrer">
                 {data.at.company}
@@ -34,17 +47,28 @@ export default function Proyecto({ data }: { data: DataProps }) {
               data.at.company
             )}
           </h2>
-          <p className="text-zinc-600 text-sm">{data.location}</p>
+          <p className="text-sm px-4">{data.location}</p>
         </div>
         <div>
-          <span className="text-sm text-zinc-700 dark:text-zinc-400">
-            {data.description}
-          </span>
+          <span className="block text-sm px-4 pb-4">{data.description}</span>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 pt-2">
             {data.skills.map((skill, index) => (
               <span
                 key={skill.name}
-                className={`text-sm text-zinc-700 dark:text-zinc-400 inline-block hover:text-current-skill-color transition-all ease-in duration-200`}
+                className={`
+                  text-sm block px-4 py-2
+                  hover:bg-current-skill-color
+                  transition-all ease-in duration-200
+                  border-t border-[#a9b4c6] border-dashed
+
+                  ${(index + 1) % 2 === 0 ? "border-r-0" : "border-r"}
+                  ${(index + 1) % 3 === 0 ? "sm:border-r-0" : "sm:border-r"}
+                  ${(index + 1) % 4 === 0 ? "md:border-r-0" : "md:border-r"}
+
+                  ${hasBorder(index, 2) ? "border-b" : "border-b-0"}
+                  ${hasBorder(index, 3) ? "sm:border-b" : "sm:border-b-0"}
+                  ${hasBorder(index, 4) ? "md:border-b" : "md:border-b-0"}
+                `}
                 style={
                   {
                     "--skill-color": skill.color,
--- src/components/main/menu.tsx
+++ src/components/main/menu.tsx
@@ -3,50 +3,59 @@ import ThemeButton from "./theme_button";
 import UpButton from "./up_button";
 import TranslateButton, { type TranslateButtonHandle } from "./translate";
 
-export default function NavMenu({ actualpathEn, actualpathEs } : { actualpathEn: string; actualpathEs: string }) {
-    const [open, setOpen] = useState(false);
-    const themeButtonRef = useRef<HTMLButtonElement>(null);
-    const upButtonRef = useRef<HTMLButtonElement>(null);
-    const translateButtonRef = useRef<TranslateButtonHandle>(null);
+export default function NavMenu({
+  actualpathEn,
+  actualpathEs,
+}: {
+  actualpathEn: string;
+  actualpathEs: string;
+}) {
+  const [open, setOpen] = useState(false);
+  const themeButtonRef = useRef<HTMLButtonElement>(null);
+  const upButtonRef = useRef<HTMLButtonElement>(null);
+  const translateButtonRef = useRef<TranslateButtonHandle>(null);
 
+  useEffect(() => {
+    if (open) {
+      if (themeButtonRef.current) {
+        themeButtonRef.current.style.top = "-45px";
+      }
+      if (upButtonRef.current) {
+        upButtonRef.current.style.left = "-45px";
+      }
+      if (translateButtonRef.current) {
+        translateButtonRef.current.doShow();
+      }
+    } else {
+      if (themeButtonRef.current) {
+        themeButtonRef.current.style.top = "0px";
+      }
+      if (upButtonRef.current) {
+        upButtonRef.current.style.left = "0px";
+      }
+      if (translateButtonRef.current) {
+        translateButtonRef.current.doHide();
+      }
+    }
+  }, [open]);
 
-    useEffect(() => {
-        if (open) {
-            if (themeButtonRef.current) {
-                themeButtonRef.current.style.top = "-45px";
-            }
-            if (upButtonRef.current) {
-                upButtonRef.current.style.left = "-45px";
-            }
-            if (translateButtonRef.current) {
-                translateButtonRef.current.doShow();
-            }
-        } else {
-            if (themeButtonRef.current) {
-                themeButtonRef.current.style.top = "0px";
-            }
-            if (upButtonRef.current) {
-                upButtonRef.current.style.left = "0px";
-            }
-            if (translateButtonRef.current) {
-                translateButtonRef.current.doHide();
-            }
-        }
-    }, [open]);
-
-    return (
-        <div className="fixed right-[16px] bottom-[16px]">
-            <div className="relative">
-                <button 
-                    className="bg-zinc-300 dark:bg-zinc-600 shadow px-3 py-2 rounded-sm"
-                    onClick={() => setOpen(!open)}
-                >
-                    <i className={open ? "nf nf-fa-close" : "nf nf-md-menu"}></i>
-                </button>
-                <ThemeButton ref={themeButtonRef} />
-                <UpButton ref={upButtonRef} />
-                <TranslateButton ref={translateButtonRef} urlEs={actualpathEs} urlEn={actualpathEn} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="fixed right-[16px] bottom-[16px]">
+      <div className="relative">
+        <button
+          className="bg-zinc-300 dark:bg-zinc-600 px-3 py-2 rounded-sm"
+          onClick={() => setOpen(!open)}
+        >
+          <i className={open ? "nf nf-fa-close" : "nf nf-md-menu"}></i>
+        </button>
+        <ThemeButton ref={themeButtonRef} />
+        <UpButton ref={upButtonRef} />
+        <TranslateButton
+          ref={translateButtonRef}
+          urlEs={actualpathEs}
+          urlEn={actualpathEn}
+        />
+      </div>
+    </div>
+  );
 }
